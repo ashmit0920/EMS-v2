93e43dff6f28968458e3f5f81140b860327b7ada
unknown <ashmitthawait2@gmail.com>
2024-07-11 23:59:48 +0530

    Added unique qr code for attendees

--- attendees/urls.py
+++ attendees/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register_attendee, success
+from .views import register_attendee, success, verify_qr
 
 urlpatterns = [
     path('register/', register_attendee, name='register'),
     path('success/', success, name='success'),
+    path('verify/', verify_qr, name='verify')
 ]
--- attendees/views.py
+++ attendees/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .forms import AttendeeForm
 from .utils import get_db
+from bson.objectid import ObjectId
+import qrcode
+import io
+import base64
+
+def generate_qr(data):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image()
+    buffer = io.BytesIO
+    img.save(buffer, format="PNG")
+    return buffer.getvalue()
 
 def register_attendee(request):
     if request.method == 'POST':
@@ -12,12 +31,40 @@ def register_attendee(request):
                 'phone': form.cleaned_data['phone']
             }
             db = get_db()
-            db.attendees.insert_one(attendee_data)
+            attendee_id = db.attendees.insert_one(attendee_data).inserted_id
 
-            return redirect('success')  # Redirect to a success page
+            # Generate the QR
+            qr_code_data = str(attendee_id)
+            qr_code_img = generate_qr(qr_code_data)
+
+            # Saving the QR in database
+            db.attendees.update_one({'_id': attendee_id}, {'$set': {'qr_code': qr_code_img}})
+
+            return redirect('success')  # Redirect to success page
     else:
         form = AttendeeForm()
+
     return render(request, 'register.html', {'form': form})
 
 def success(request):
     return render(request, 'success.html')
+
+def verify_qr(request):
+    if request.method == 'POST':
+        qr_code_data = request.POST.get('qr_code_data')
+
+        if qr_code_data:
+            try:
+                db = get_db()
+                attendee = db.attendees.find_one({'_id': ObjectId(qr_code_data)})
+                
+                if attendee:
+                    return HttpResponse("Verified: " + attendee['name'])
+                
+                else:
+                    return HttpResponse("Not Verified")
+            
+            except Exception as e:
+                return HttpResponse("Invalid QR code")
+    
+    return render(request, 'verify.html')
